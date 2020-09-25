fb1316f355beb57394864adc1240e8174632a1db
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-09-25 20:27:56 +0200

    restructure

---
+++ knowledge/logic.py
@@ -0,0 +1,88 @@
+import itertools
+
+
+class Sentence():
+    
+    def evaluate(self, model):
+        raise Exception("nothing to evaluate")
+
+    def formula(self):
+        return ""
+
+    def symbols(self):
+        return set()
+
+    @classmethod
+    def validate(cls, sentence):
+        if not isinstance(sentence, Sentence):
+            raise TypeError("must be a logical sentence")
+
+    @classmethod
+    def paranthesize(cls, s):
+        def balanced(s):
+            count = 0
+            for c in s:
+                if c == "(":
+                    count += 1
+                elif c == ")":
+                    if count <= 0:
+                        return False
+                    count -= 1
+            return count == 0
+        if not len(s) or s.isalpha() or (
+            s[0] == "(" and s[-1] == ")" and balanced(s[1:-1])
+        ):
+            return s
+        else:
+            return f"({s})"
+
+
+class Symbol(Sentence):
+    
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        return isinstance(other, Symbol) and self.name == other.name
+
+    def __hash__(self):
+        return hash(("symbol", self.name))
+
+    def __repr__(self):
+        return self.name
+
+    def evaluate(self, model):
+        try:
+            return bool(model[self.name])
+        except KeyError:
+            raise EvaluationException(f"variable {self.name} not in model")
+
+    def formula(self):
+        return self.name
+
+    def symbols(self):
+        return {self.name}
+
+
+class Not(Sentence):
+    pass
+
+
+class And(Sentence):
+    pass
+
+
+class Or(Sentence):
+    pass
+
+
+class Implication():
+    pass
+
+
+class Biconditional():
+    pass
+
+
+def model_check(knowledge, query):
+    pass
